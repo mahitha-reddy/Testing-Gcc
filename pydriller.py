c2cbf7742dbcdc0a4fb65b5f48933e1277b224b6
mahitha-reddy <mahithareddy.ls37@gmail.com>
2022-05-01 20:55:07 -0500

    mahitha-reddy,sai sree,chaitanya

--- pydriller.py
+++ pydriller.py
@@ -2,7 +2,9 @@ from pydriller import Repository
 
 for commit in Repository('https://github.com/mahitha-reddy/Testing-Gcc').traverse_commits():
     print(commit.hash)
+
     print(commit.msg)
+
     print(commit.author.name)
 
     for file in commit.modified_files:
